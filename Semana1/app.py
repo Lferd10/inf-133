3f99fa7411dbe5888cc4534ba4a7e953eb585146
Lferd10 <luisfernandovallejoscopa0@gmail.com>
2024-02-27 22:29:37 -0400

    update

--- Semana1/app.py
+++ Semana1/app.py
@@ -4,12 +4,11 @@ def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
     try:
         server_address = ('', 8000)
         httpd = server_class(server_address, handler_class)
-        print("iniciando servidor web en http://localhost:8000/")
+        print('Iniciando servidor web en http://localhost:8000/')
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print('apagando servidor web')
+        print('Apagando servidor web')
         httpd.socket.close()
 
-
-if __name__ == "_main_":
+if __name__ == "__main__":
     run()
