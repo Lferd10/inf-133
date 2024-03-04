cc0eae3abdf8b943d626fc9cf703a3cc090de585
Lferd10 <155495125+Lferd10@users.noreply.github.com>
2024-03-04 17:11:02 +0000

    update

--- Semana2/restful/server.py
+++ Semana2/restful/server.py
@@ -12,82 +12,72 @@ estudiantes = [
 
 
 class RESTRequestHandler(BaseHTTPRequestHandler):
+    
+    def response_handler(self, status, data):
+        self.send_response(status)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode("utf-8"))
+
+    def find_student(self, id):
+        return next(
+            (estudiante for estudiante in estudiantes if estudiante["id"] == id),
+            None,
+        )
+
+    def read_data(self):
+        content_length = int(self.headers["Content-Length"])
+        data = self.rfile.read(content_length)
+        data = json.loads(data.decode("utf-8"))
+        return data
+
     def do_GET(self):
         if self.path == "/estudiantes":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(estudiantes).encode("utf-8"))
+
         elif self.path.startswith("/estudiantes/"):
             id = int(self.path.split("/")[-1])
-            estudiante = next(
-                (estudiante for estudiante in estudiantes if estudiante["id"] == id),
-                None,
-            )
+            estudiante = self.find_student(id)
             if estudiante:
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(estudiante).encode("utf-8"))
+                self.response_handler(200, [estudiante])
+            else:
+                self.response_handler(204, [])
 
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"Error": "Ruta no existente"}).encode("utf-8"))
+            self.response_handler(404, {"Error": "Ruta no existente"})
 
     def do_POST(self):
         if self.path == "/estudiantes":
-            content_length = int(self.headers["Content-Length"])
-            post_data = self.rfile.read(content_length)
-            post_data = json.loads(post_data.decode("utf-8"))
-            post_data["id"] = len(estudiantes) + 1
-            estudiantes.append(post_data)
-            self.send_response(201)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(estudiantes).encode("utf-8"))
+            data = self.read_data()
+            data["id"] = len(estudiantes) + 1
+            estudiantes.append(data)
+            self.response_handler(201, estudiantes)
 
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"Error": "Ruta no existente"}).encode("utf-8"))
+            self.response_handler(404, {"Error": "Ruta no existente"})
 
     def do_PUT(self):
-        if self.path.startswith("/estudiantes"):
-            content_length = int(self.headers["Content-Length"])
-            data = self.rfile.read(content_length)
-            data = json.loads(data.decode("utf-8"))
-            id = data["id"]
-            estudiante = next(
-                (estudiante for estudiante in estudiantes if estudiante["id"] == id),
-                None,
-            )
+        if self.path.startswith("/estudiantes/"):
+            id = int(self.path.split("/")[-1])
+            estudiante = self.find_student(id)
+            data = self.read_data()
             if estudiante:
                 estudiante.update(data)
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(estudiante).encode("utf-8"))
+                self.response_handler(200, [estudiantes])
+            else:
+                self.response_handler(404, {"Error": "Estudiante no encontrado"})
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"Error": "Ruta no existente"}).encode("utf-8"))
+            self.response_handler(404, {"Error": "Ruta no existente"})
 
     def do_DELETE(self):
         if self.path == "/estudiantes":
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
             estudiantes.clear()
-            self.wfile.write(json.dumps(estudiantes).encode("utf-8"))
+            self.response_handler(200, estudiantes)
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"Error": "Ruta no existente"}).encode("utf-8"))
+            self.response_handler(404, {"Error": "Ruta no existente"})
 
 
 def run_server(port=8000):
