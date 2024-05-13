96ad89ebe6485bd403c618fde93e7723731ef72b
Lferd10 <155495125+Lferd10@users.noreply.github.com>
2024-05-13 16:55:07 +0000

    update

---
+++ test.py
@@ -0,0 +1,23 @@
+from functools import wraps
+
+def my_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("antes de llamar a la funcion")
+        result = func(*args, **kwargs)
+        print("Despues de llamar a la funcion")
+        return result
+    return wrapper
+
+
+@my_decorator
+def greet(name):
+    """Funcion para saludar a alguien"""
+    return f"Hola, {name}!"
+
+greet("Luis")
+
+print(greet.__name__)
+print(greet.__doc__)
+
+
